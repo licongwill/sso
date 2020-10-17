f9d57987437bdf9200bccfffbf0d2d9e44108167
licong <licongcong.lcc@sunyur.com>
2020-10-17 17:15:09 +0800

    add Authentication

---
+++ sso-api/src/main/java/com/lic/authentication/adaptive/UnauthorizedAuthenticationException.java
@@ -0,0 +1,33 @@
+package com.lic.authentication.adaptive;
+
+import com.lic.authentication.AuthenticationException;
+import com.lic.authentication.HandlerResult;
+
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * @author licong
+ * @version 1.0
+ * @date 2020/10/17 15:25
+ */
+public class UnauthorizedAuthenticationException extends AuthenticationException {
+
+    public UnauthorizedAuthenticationException(final String message, final Map<String, Class<? extends Exception>> handlerErrors) {
+        super(message, handlerErrors, Collections.emptyMap());
+    }
+
+    public UnauthorizedAuthenticationException(final Map<String, Class<? extends Exception>> handlerErrors) {
+        super(handlerErrors);
+    }
+
+    public UnauthorizedAuthenticationException(final Map<String, Class<? extends Exception>> handlerErrors, final Map<String, HandlerResult> handlerSuccesses) {
+        super(handlerErrors, handlerSuccesses);
+    }
+
+    public UnauthorizedAuthenticationException(final String message,
+                                               final Map<String, Class<? extends Exception>> handlerErrors,
+                                               final Map<String, HandlerResult> handlerSuccesses) {
+        super(message, handlerErrors, handlerSuccesses);
+    }
+}
