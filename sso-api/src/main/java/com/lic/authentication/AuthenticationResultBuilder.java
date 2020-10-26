6d538ff35407d1eec0b4f76ab791fb803b24ed1a
licong <licongcong.lcc@sunyur.com>
2020-10-26 20:24:27 +0800

    修改AuthenticationTransaction

---
+++ sso-api/src/main/java/com/lic/authentication/AuthenticationResultBuilder.java
@@ -0,0 +1,31 @@
+package com.lic.authentication;
+
+import java.io.Serializable;
+import java.util.Optional;
+
+/**
+ * @author licong
+ * @version 1.0
+ * @date 2020/10/26 20:09
+ */
+public interface AuthenticationResultBuilder extends Serializable {
+    /**
+     * get the init authentication
+     * @return
+     */
+    Optional<Authentication> getInitAuthentication();
+
+    /**
+     * coleect authentication
+     * @param authentication
+     * @return
+     */
+    AuthenticationResultBuilder collect(Authentication authentication);
+
+    /**
+     * collect credential
+     * @param credential
+     * @return
+     */
+    AuthenticationResultBuilder collect(Credential credential);
+}
