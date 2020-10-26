e023f026b85faab3f08a5ecce23f162046a055b8
licong <licongcong.lcc@sunyur.com>
2020-10-26 18:10:20 +0800

    修改AuthenticationTransaction

--- sso-api/src/main/java/com/lic/authentication/AuthenticationTransaction.java
+++ sso-api/src/main/java/com/lic/authentication/AuthenticationTransaction.java
@@ -4,7 +4,6 @@ import com.lic.authentication.principal.Service;
 
 import java.io.Serializable;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
