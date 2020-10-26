4a2b6d8d8341572e362a05c75cacb143484d9912
licong <licongcong.lcc@sunyur.com>
2020-10-26 20:29:39 +0800

    修改AuthenticationTransaction

--- sso-api/src/main/java/com/lic/authentication/AuthenticationResultBuilder.java
+++ sso-api/src/main/java/com/lic/authentication/AuthenticationResultBuilder.java
@@ -1,5 +1,7 @@
 package com.lic.authentication;
 
+import com.lic.authentication.principal.Service;
+
 import java.io.Serializable;
 import java.util.Optional;
 
@@ -28,4 +30,20 @@ public interface AuthenticationResultBuilder extends Serializable {
      * @return
      */
     AuthenticationResultBuilder collect(Credential credential);
+
+    /**
+     * build authentcation result
+     * @param principalElectionStrategy
+     * @return
+     */
+    AuthenticationResult build(PrincipalElectionStrategy principalElectionStrategy);
+
+
+    /**
+     * build authentication result
+     * @param principalElectionStrategy
+     * @param service
+     * @return
+     */
+    AuthenticationResult build(PrincipalElectionStrategy principalElectionStrategy, Service service);
 }
